4889d071fe06c283d6b783c91ee715c7c3b5b8b2
Joe Burton <joeburton@gmail.com>
2022-01-17 16:28:48 +0000

    update server

--- api/index.js
+++ api/index.js
@@ -6,34 +6,24 @@ import { ApolloServer } from 'apollo-server-express';
 import typeDefs from './typedefs/index.js';
 import resolvers from './resolvers/index.js';
 
-async function startServer() {
-  const app = express();
+const app = express();
 
-  app.use(cors());
-  app.use(express.json());
+app.use(cors());
+app.use(express.json());
 
-  const httpServer = createServer(app);
+const httpServer = createServer(app);
 
+const startApolloServer = async (app, httpServer) => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
 
   await server.start();
-
   server.applyMiddleware({ app });
-
-  await new Promise((resolve) => httpServer.listen({ port: 3000 }, resolve));
-
-  console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`);
-
-  return { server, app };
-}
-
-startServer();
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
 };
+
+startApolloServer(app, httpServer);
+
+// Export the http server so that Vercel can start it:
+export default httpServer;
